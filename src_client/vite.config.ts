5938126d9e4c2b99e1b9f8a304c7a47531786e59
ish <ish@ish.works>
2022-01-30 01:49:09 +0200

    silly workaround for scss warnings

--- src_client/vite.config.ts
+++ src_client/vite.config.ts
@@ -8,5 +8,21 @@ export default defineConfig({
       proxy: {
         '/api': 'http://127.0.0.1:8080' // proxy api requests to cherrypy during dev
       }
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            }
+          }
+        }
+      ]
+    }
+}
 });
