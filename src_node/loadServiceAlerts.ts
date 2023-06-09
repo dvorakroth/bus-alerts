c7a911d409f809384c15aff108c4db6398cb6140
ish <ish@ish.works>
2023-06-09 17:19:52 +0300

    refactor tryParseFilenameDate into its own file

--- src_node/loadServiceAlerts.ts
+++ src_node/loadServiceAlerts.ts
@@ -120,24 +120,3 @@ async function main() {
 
 main();
 
-const FILENAME_DATE_REGEX = /(?<year>\d+)\D(?<month>\d+)\D(?<day>\d+)\D(?<hour>\d+)\D(?<minute>\d+)\D(?<second>\d+)/g;
-function tryParseFilenameDate(filename: string): DateTime|null {
-    const match = FILENAME_DATE_REGEX.exec(filename);
-    if (!match) {
-        return null;
-    }
-
-    const result = DateTime.fromObject(
-        // the regex is ~*~perfectly engineered~*~ for the named groups to match lol
-        match.groups as any,
-        { zone: JERUSALEM_TZ }
-    );
-
-    if (!result.isValid) {
-        winston.warn(`couldn't make a date out of numbers in filename: ${filename}\n${result.invalidExplanation}`);
-        return null;
-    } else {
-        winston.info(`found date ${result.toFormat(TIME_FORMAT_ISO_NO_TZ)} in filename ${filename}`);
-        return result;
-    }
-}
