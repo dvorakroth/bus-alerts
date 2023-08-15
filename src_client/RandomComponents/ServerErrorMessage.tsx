d6297f3a07f1645398cf18054aa8e1506e90bc5a
ish <ish@ish.works>
2023-08-15 17:16:23 +0300

    added a new error view specifically for when /api/get_route_changes returns a 500 or whatever

--- src_client/RandomComponents/ServerErrorMessage.tsx
+++ src_client/RandomComponents/ServerErrorMessage.tsx
@@ -13,3 +13,18 @@ export const ServerErrorMessage = () => <>
         אם זה נשאר)
     </span>
 </>;
+
+export const ServerErrorChangesMessage = () => <>
+    <div className="no-alerts-today error-loading-changes">
+        <span>כאן אמור להופיע עוד מידע</span>
+        <span>אבל במקום זה יש שגיאה</span>
+        <span className="snarky-comment">
+            אפשר לנסות שוב; אולי זה יעזור?
+            <br />
+            (ואפשר גם
+            {" "}
+            <a href="mailto:ish@ish.works">לכתוב לי</a>,
+            אם השגיאה הזו תחזור)
+        </span>
+    </div>
+</>;
